f1b89ad99ced73418cf05e5a85dd4d1a9132f0be
Paulo Henrique Ferreira <127543378+askot19@users.noreply.github.com>
2023-10-03 10:43:00 -0300

    Update harrypotter.sql

--- HackerRank/harrypotter.sql
+++ HackerRank/harrypotter.sql
@@ -1,3 +1,5 @@
+--Using subquery
+
 SELECT w.id
     , wp.age
     , w.coins_needed
@@ -8,4 +10,29 @@ AND w.code = g.code
 AND w.coins_needed = g.min_price
 AND wp.is_evil = 0
 AND wp.code = g.code
-ORDER BY w.power DESC, wp.age DESC;  
+ORDER BY w.power DESC, wp.age DESC;
+
+--Using JOIN
+
+SELECT
+  w.id,
+  wp.age,
+  w.coins_needed,
+  w.power
+  
+FROM
+  wands AS w
+JOIN
+  wands_property AS wp ON w.code = wp.code
+JOIN
+  (SELECT power, code, MIN(coins_needed) AS min_price FROM wands GROUP BY power, code) AS g
+ON
+  wp.code = g.code
+  AND w.coins_needed = g.min_price
+
+WHERE
+  wp.is_evil = 0
+
+ORDER BY
+  w.power DESC,
+  wp.age DESC;
